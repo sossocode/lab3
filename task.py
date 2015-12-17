4bd128ca1c10e8e7215e544d5d76af028078ef1e
sossocode <sofia.balter@hotmail.se>
2015-12-17 10:04:16 +0100

    Update task.py

--- task.py
+++ task.py
@@ -6,16 +6,6 @@ import subprocess
 from celery import group
 import time
 
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def cow_say():
-    data = count()
-    return data
-
-if __name__ == '__main__':    
-    app.run(host='0.0.0.0',debug=True)
-
 
 app  = Celery('tasks', backend= 'amqp', broker = 'amqp://')
 
